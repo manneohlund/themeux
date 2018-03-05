a0b39b7f59718849585d063cefdea99c2a67d392
manneohlund <manne.ohlund@gmail.com>
2018-03-05 21:33:48 +0100

    [REF] Optimised if to Kotlin when statements

--- themeux/src/main/java/themeux/util/ReflectionUtil.kt
+++ themeux/src/main/java/themeux/util/ReflectionUtil.kt
@@ -13,12 +13,10 @@ import kotlin.reflect.jvm.isAccessible
 object ReflectionUtil {
     private fun getValue(item: KCallable<*>, instance: Any): Any {
         item.isAccessible = true
-        if (item is KFunction) {
-            return (item as KFunction<Any>).call(instance)
-        } else if (item is KProperty) {
-            return (item as KProperty1<Any, Any>).get(instance)
-        } else {
-            throw Exception("item is not a function or var/val")
+        return when (item) {
+            is KFunction -> (item as KFunction<Any>).call(instance)
+            is KProperty -> (item as KProperty1<Any, Any>).get(instance)
+            else -> throw Exception("item is not a function or var/val")
         }
     }
 
@@ -27,15 +25,16 @@ object ReflectionUtil {
         val members = themeModel.javaClass.kotlin.declaredMembers.filter {
             it.annotations.any { it.annotationClass == annotationClass }
         }
-        if (members.size > 1) {
-            throw Exception("Error! Add @${annotationClass.simpleName} annotation to only one field or function")
-        } else if (!isOptional && members.isEmpty()) {
-             throw NoSuchElementException("${annotationClass.simpleName} annotation not found! Add @${annotationClass.simpleName} annotation to only one field or function")
-        } else if (isOptional && members.isEmpty()) {
-            Log.e(javaClass.name, "Optional ${annotationClass.simpleName} annotation not found!")
-            return null
-        } else {
-            return getValue(members.first(), themeModel)
+        when {
+            members.size > 1 ->
+                throw Exception("Error! Add @${annotationClass.simpleName} annotation to only one field or function")
+            !isOptional && members.isEmpty() ->
+                throw NoSuchElementException("${annotationClass.simpleName} annotation not found! Add @${annotationClass.simpleName} annotation to only one field or function")
+            isOptional && members.isEmpty() -> {
+                Log.e(javaClass.name, "Optional ${annotationClass.simpleName} annotation not found!")
+                return null
+            }
+            else -> return getValue(members.first(), themeModel)
         }
     }
 }
